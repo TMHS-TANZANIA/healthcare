b9543c9805c4797d4c78db7c89ab724f1ef1a023
spectre720 <amanimgonja112@gmail.com>
2025-07-25 21:59:22 +0300

    Remove the custom button for generating weekly reports from the Site Daily Report form.

--- healthcare/healthcare/doctype/site_daily_report/site_daily_report.js
+++ healthcare/healthcare/doctype/site_daily_report/site_daily_report.js
@@ -7,30 +7,3 @@
 // 	},
 // });
 
-frappe.ui.form.on("Site daily report", {
-    refresh(frm) {
-        if (frm.doc.docstatus === 1 && frm.doc.site_name && frm.doc.date) {
-            frm.add_custom_button(
-                __("Generate Weekly Report"),
-                function () {
-                    frappe.call({
-                        method: "healthcare.healthcare.doctype.site_weekly_report.site_weekly_report.generate_weekly_report_for_site_and_date",
-                        args: {
-                            site: frm.doc.site_name,
-                            date: frm.doc.date
-                        },
-                        callback: function(r) {
-                            if (r.message) {
-                                frappe.set_route("Form", "Site Weekly Report", r.message);
-                            } else {
-                                frappe.msgprint(__("Weekly report generated/updated."));
-                            }
-                        }
-                    });
-                },
-                __("Actions")
-            );
-        }
-    }
-});
-
