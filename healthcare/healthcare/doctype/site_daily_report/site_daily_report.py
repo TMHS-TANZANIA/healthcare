196e0ce20eb580867f1167c4027929532f9de53d
spectre720 <amanmgonja112@gmail.com>
2025-08-08 20:15:28 +0300

    feat: Add functionality to return selected site daily reports to draft status with user confirmation

--- healthcare/healthcare/doctype/site_daily_report/site_daily_report.py
+++ healthcare/healthcare/doctype/site_daily_report/site_daily_report.py
@@ -1,9 +1,27 @@
-# Copyright (c) 2025, earthians Health Informatics Pvt. Ltd. and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe import _
 
+class SiteDailyReport(Document):
+    pass
 
-class Sitedailyreport(Document):
-	pass
+@frappe.whitelist()
+def return_for_edit(docs):
+    if isinstance(docs, str):
+        import json
+        docs = json.loads(docs)
+
+    for name in docs:
+        doc = frappe.get_doc("site daily report", name)
+
+        # Check if user has Manager role
+        if "Site Report Manager" not in frappe.get_roles(frappe.session.user):
+            frappe.throw(_("You are not authorized to perform this action."))
+
+        # Only allow if submitted
+        if doc.docstatus == 1:
+            doc.docstatus = 0  # Draft
+            doc.save(ignore_permissions=True)
+            frappe.db.commit()
+        else:
+            frappe.msgprint(_("Document {0} is not submitted.").format(name))
--- healthcare/healthcare/doctype/site_daily_report/site_daily_report_list.js
+++ healthcare/healthcare/doctype/site_daily_report/site_daily_report_list.js
@@ -1,4 +1,4 @@
-frappe.listview_settings['Site daily report'] = {
+frappe.listview_settings['site daily report'] = {
     onload: function(listview) {
         listview.page.add_inner_button(__('Generate Weekly Reports (All Sites)'), function() {
             frappe.call({
@@ -9,5 +9,31 @@ frappe.listview_settings['Site daily report'] = {
                 }
             });
         });
-    }
-}; 
+        listview.page.add_actions_menu_item(__('Return for Edit'), function() {
+            let selected = listview.get_checked_items();
+
+            if (!selected.length) {
+                frappe.msgprint(__('Please select at least one document.'));
+                return;
+            }
+
+            frappe.confirm(
+                __('Are you sure you want to return the selected documents to Draft?'),
+                function() {
+                    frappe.call({
+                        method: 'healthcare.healthcare.doctype.site_daily_report.site_daily_report.return_for_edit',
+                        args: {
+                            docs: selected.map(d => d.name)
+                        },
+                        callback: function(r) {
+                            if (!r.exc) {
+                                frappe.msgprint(__('Selected documents have been returned to Draft.'));
+                                listview.refresh();
+                            }
+                        }
+                    });
+                }
+            );
+        });
+    },
+};
